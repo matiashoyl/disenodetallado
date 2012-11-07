525ce49df020b0dc94c681c10031c3283cd6b9d0
Matias Hoyl <matiashoyl@gmail.com>
2012-11-07 20:36:04 -0300

    Main más comentado

--- Main.java
+++ Main.java
@@ -4,10 +4,14 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		
+		//Creamos un nuevo comunicador pasándole como parámetro su id
 		Comunication c = new Comunication(6005);
+		//Comenzamos el thread que levantará el servidor
 		new Thread(c).start();
+		//Intentamos conectarnos a la sesión
 		c.connectToSession();
 		
+		//Loop infinito que recibe input del teclado y lo envía al nodo indicado
 		while(true)
 		{
 			BufferedReader inFromUser = 
@@ -15,6 +19,7 @@ public class Main {
 
 			String sentenceConsole = inFromUser.readLine();
 			
+			//Ejemplo de uso del metodo para enviar mensaje. El número dos indica el caracter de mensaje normal
 			c.sendMessage("2_"+sentenceConsole, 6003);
 		}
 
